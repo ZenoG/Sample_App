4042b819680847d188ce1abac75900f096e3e08b
ZenoG <mbomb002@gmail.com>
2013-11-21 12:42:37 -0500

    Made minor changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,8 @@ class StaticPagesController < ApplicationController
   def home
     @micropost = current_user.microposts.build if signed_in?
     @feed_items = current_user.feed.paginate(page: params[:page])
-  end
+end
+
 
   def help
   end
