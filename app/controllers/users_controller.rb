ceee57211e2872cb14350f4ebe1aa5acc59c7ba7
ZenoG <mbomb002@gmail.com>
2013-11-20 19:08:52 -0500

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ before_action :admin_user,     only: :destroy
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -54,12 +55,7 @@ before_action :admin_user,     only: :destroy
 
     # Before filters
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+   
 
     def correct_user
       @user = User.find(params[:id])
